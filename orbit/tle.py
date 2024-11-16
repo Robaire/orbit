faa7527808f8bcc041a43df6b0ca71ff12071751
Robaire Galliath <galliath.robaire@gmail.com>
2024-11-15 23:48:39 -0500

    Implement to_string and add checksum validation

--- orbit/tle.py
+++ orbit/tle.py
@@ -1,11 +1,11 @@
 # Robaire Galliath, 2024
 
+from __future__ import annotations
 from dataclasses import dataclass
 
 
 @dataclass
 class TLE:
-
     # Title (Line 0)
     title: str
 
@@ -32,21 +32,34 @@ class TLE:
     mean_motion: float  # [rev/day]
     revolutions_epoch: int  # [1]
 
+    @staticmethod
+    def _checksum(line: str) -> int:
+        """Calculate the checksum of a TLE line.
+
+        Args:
+            line: The line to calculate the checksum of.
+            If validating a complete line, omit the checksum (last) character
+
+        Returns:
+            checksum: The checksum value
+        """
+        return sum([int(c) for c in line.replace("-", "1") if c.isdigit()]) % 10
+
     @classmethod
-    def from_string(cls, tle_string: str):
+    def from_string(cls, tle_string: str) -> TLE:
         """Generate a TLE from a standard two or three line element set.
 
         Args:
             string: A two or three line element set
 
         Returns:
-            TLE: TLE object
+            TLE object
         """
 
         input = [x.strip() for x in tle_string.split("\n") if x.strip()]
 
         if len(input) != 2 and len(input) != 3:
-            raise ValueError("Improperly formatted TLE")
+            raise ValueError("Invalid TLE format")
 
         if len(input) == 2:
             title = ""
@@ -58,13 +71,20 @@ class TLE:
             line2 = input[2]
 
         # Extract line 1 elements
+        if cls._checksum(line1[:-1]) != int(line1[-1:]):
+            raise ValueError("Invalid checksum in line 1")
+
         line1 = [x.strip() for x in line1.split(" ") if x]
         catalog_number = int(line1[1][:5])
         classification = line1[1][5:]
         launch_year = int(line1[2][:2])
+        launch_year += 1900 if launch_year <= 99 and launch_year >= 57 else 2000
         launch_number = int(line1[2][2:5])
         launch_piece = line1[2][5:]
+
         epoch_year = int(line1[3][:2])
+        epoch_year += 1900 if epoch_year <= 99 and epoch_year >= 57 else 2000
+
         epoch_day = float(line1[3][2:])
         mean_motion_d1 = float(line1[4])
 
@@ -80,6 +100,9 @@ class TLE:
         element_number = int(line1[8][:-1])
 
         # Extract line 2 elements
+        if cls._checksum(line2[:-1]) != int(line2[-1:]):
+            raise ValueError("Invalid checksum in line 2")
+
         line2 = [x.strip() for x in line2.split(" ") if x]
         inclination = float(line2[2])
         right_ascension = float(line2[3])
@@ -113,15 +136,60 @@ class TLE:
         )
 
     @classmethod
-    def from_file(cls, file_path):
+    def from_file(cls, file_path: str) -> TLE:
         """Generate a TLE from a file.
 
         Args:
             file_path: File path
 
-        Return:
-            TLE: TLE object
+        Returns:
+            TLE object
         """
 
         with open(file_path, "r") as file:
             return TLE.from_string(file.read())
+
+    @staticmethod
+    def _zero_float(n: float) -> str:
+        """Format a float in the scientific notation used in a TLE."""
+        if n == 0:
+            return " 00000-0"
+
+        result = "" if n < 0 else " "
+        result += f"{n:.4e}".split("e")[0].replace(".", "")
+        result += str(int(f"{n:.0e}".split("e")[1]) + 1)
+        return result
+
+    def to_string(self) -> str:
+        """Generate a TLE string."""
+
+        title = self.title
+        if title != "":
+            title = title[:24] + "\n"  # limit to 24 characters
+
+        # Line 1
+        line1 = (
+            f"1 {self.catalog_number:5}{self.classification[:1]} "
+            + f"{self.launch_year}"[-2:]
+            + f"{self.launch_number:03}{self.launch_piece:3} "
+            + f"{self.epoch_year}"[-2:]
+            + f"{self.epoch_day:12.8f} "
+        )
+        line1 += "-." if self.mean_motion_d1 < 0 else " ."
+        line1 += f"{self.mean_motion_d1:.8f}".split(".")[1] + " "
+        line1 += self._zero_float(self.mean_motion_d2) + " "
+        line1 += self._zero_float(self.b_star) + " "
+        line1 += f"{self.ephemeris_type} {self.element_number:4}"
+        line1 += f"{self._checksum(line1)}\n"
+
+        # Line 2
+        line2 = (
+            f"2 {self.catalog_number:5} {self.inclination:8.4f} "
+            + f"{self.right_ascension:8.4f} "
+            + f"{self.eccentricity:.7f}".split(".")[1]
+            + f" {self.argument_perigee:8.4f} {self.mean_anomaly:8.4f} "
+            + f"{self.mean_motion:11.8f}{self.revolutions_epoch:05}"
+        )
+        line2 += f"{self._checksum(line2)}"
+
+        return title + line1 + line2
--- test/test_tle.py
+++ test/test_tle.py
@@ -5,8 +5,14 @@ import pytest
 from orbit.tle import TLE
 
 
-def test_from_string_length():
+@pytest.fixture
+def iss_tle_str():
+    return """ISS (ZARYA)
+1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
+2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""
 
+
+def test_from_string_length():
     with pytest.raises(ValueError):
         TLE.from_string("")
 
@@ -15,26 +21,20 @@ def test_from_string_length():
 
 
 def test_from_string_format():
-
     with pytest.raises(ValueError):
         TLE.from_string("BAD SAT\n1 3434N 98032A\n2 FOO BAR")
 
 
-def test_from_string_ISS():
-
-    iss_tle_str = """ISS (ZARYA)
-1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
-2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""
-
+def test_from_string_ISS(iss_tle_str):
     iss_tle = TLE.from_string(iss_tle_str)
 
     assert iss_tle.title == "ISS (ZARYA)"
     assert iss_tle.catalog_number == 25544
     assert iss_tle.classification == "U"
-    assert iss_tle.launch_year == 98
+    assert iss_tle.launch_year == 1998
     assert iss_tle.launch_number == 67
     assert iss_tle.launch_piece == "A"
-    assert iss_tle.epoch_year == 8
+    assert iss_tle.epoch_year == 2008
     assert iss_tle.epoch_day == 264.51782528
     assert iss_tle.mean_motion_d1 == -0.00002182
     assert iss_tle.mean_motion_d2 == 0.0
@@ -51,9 +51,7 @@ def test_from_string_ISS():
 
 
 def test_from_string_two_line():
-
-    iss_tle_str = """
-1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
+    iss_tle_str = """1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
 2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""
 
     iss_tle = TLE.from_string(iss_tle_str)
@@ -61,10 +59,10 @@ def test_from_string_two_line():
     assert iss_tle.title == ""
     assert iss_tle.catalog_number == 25544
     assert iss_tle.classification == "U"
-    assert iss_tle.launch_year == 98
+    assert iss_tle.launch_year == 1998
     assert iss_tle.launch_number == 67
     assert iss_tle.launch_piece == "A"
-    assert iss_tle.epoch_year == 8
+    assert iss_tle.epoch_year == 2008
     assert iss_tle.epoch_day == 264.51782528
     assert iss_tle.mean_motion_d1 == -0.00002182
     assert iss_tle.mean_motion_d2 == 0.0
@@ -80,12 +78,7 @@ def test_from_string_two_line():
     assert iss_tle.revolutions_epoch == 56353
 
 
-def test_from_file(fs):
-
-    iss_tle_str = """ISS (ZARYA)
-1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
-2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""
-
+def test_from_file(fs, iss_tle_str):
     fs.create_file("iss_tle.txt", contents=iss_tle_str)
 
     iss_tle = TLE.from_file("iss_tle.txt")
@@ -93,10 +86,10 @@ def test_from_file(fs):
     assert iss_tle.title == "ISS (ZARYA)"
     assert iss_tle.catalog_number == 25544
     assert iss_tle.classification == "U"
-    assert iss_tle.launch_year == 98
+    assert iss_tle.launch_year == 1998
     assert iss_tle.launch_number == 67
     assert iss_tle.launch_piece == "A"
-    assert iss_tle.epoch_year == 8
+    assert iss_tle.epoch_year == 2008
     assert iss_tle.epoch_day == 264.51782528
     assert iss_tle.mean_motion_d1 == -0.00002182
     assert iss_tle.mean_motion_d2 == 0.0
@@ -110,3 +103,31 @@ def test_from_file(fs):
     assert iss_tle.mean_anomaly == 325.0288
     assert iss_tle.mean_motion == 15.72125391
     assert iss_tle.revolutions_epoch == 56353
+
+
+def test_to_string(iss_tle_str):
+    assert TLE.from_string(iss_tle_str).to_string() == iss_tle_str
+
+    tle_str = """1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
+2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""
+    assert TLE.from_string(tle_str).to_string() == tle_str
+
+    tle_str = """1 25544U 98067A   04236.56031392  .00020137  00000-0  16538-3 0  9993
+2 25544  51.6335 344.7760 0007976 126.2523 325.9359 15.70406856328906"""
+    assert TLE.from_string(tle_str).to_string() == tle_str
+
+
+def test_checksum():
+    tle_str = """1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2929
+2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537"""
+
+    with pytest.raises(ValueError) as exc:
+        TLE.from_string(tle_str)
+        assert exc.value == "Invalid checksum in line 1"
+
+    tle_str = """1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927
+2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563539"""
+
+    with pytest.raises(ValueError) as exc:
+        TLE.from_string(tle_str)
+        assert exc.value == "Invalid checksum in line 2"
